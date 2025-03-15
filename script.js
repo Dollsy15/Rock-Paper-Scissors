ff665c2b8865bb06526a3579c71d20423fe45d0f
Dollsy Rani <dollsyrani155@gmail.com>
2025-03-15 21:01:57 +0530

    js

--- script.js
+++ script.js
@@ -3,9 +3,45 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const genComputerchoice = () => {
+    // rock, paper, scissors
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+}
+
+const drawGame = () =>{
+    console.log("Game was draw")
+}
+
+const playGame = () => {
+    console.log("user choice = ", userChoice);
+
+    // generate computer choice -> modular way of programming(reusable components)
+    const compChoice = genComputerchoice();
+    console.log("computer choice = ", compChoice);
+
+    if(userChoice === compChoice){
+        // draw game
+        drawGame();
+    }
+    else{
+        let userWin = true;
+        if(userChoice === "rock"){
+            // scissors, paper
+            compChoice === "paper" ? false : true;
+        } 
+        else if(userChoice == "paper"){
+            
+        }
+    }
+};
+
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
-        const choiceId = choice.getAttribute("id");
-        console.log("Choice was clicked", choiceId);
+        const userChoice = choice.getAttribute("id");
+        console.log("Choice was clicked", userChoice);
+        playGame();
     });
 });
+
